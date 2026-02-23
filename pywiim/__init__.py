242cb16aa24f7df4f0c5ec003448e1f00312d8ad
mjcumming <mike@4831.com>
2026-02-22 22:26:22 -0600

    chore: bump version to 2.1.85

--- pywiim/__init__.py
+++ pywiim/__init__.py
@@ -72,7 +72,7 @@ from .profiles import (
 from .role import RoleDetectionResult, detect_role
 from .state import GroupStateSynchronizer, StateSynchronizer
 
-__version__ = "2.1.84"
+__version__ = "2.1.85"
 __all__ = [
     # Main client
     "WiiMClient",
